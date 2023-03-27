d7f3c80f03b296ed2d02a856bb7fb8a239480f75
gonza rolo <gonzarolo22@gmail.com>
2023-03-27 11:12:44 -0300

    cree la entidad Fixed Term

---
+++ src/main/java/com/alkemy/wallet/entity/FixedTermDeposits.java
@@ -0,0 +1,41 @@
+package com.alkemy.wallet.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class FixedTermDeposits {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "AMOUNT", nullable = false)
+    private Double amount;
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account accountId;
+    @Column(name = "INTEREST", nullable = false)
+    private Double interest;
+
+    @CreationTimestamp
+    @Column(name="CREATION_DATE")
+    @JsonFormat(pattern="yyyy-MM-dd")
+    LocalDateTime creationDate;
+
+    @UpdateTimestamp
+    @Column(name="UPDATE_DATE")
+    @JsonFormat(pattern="yyyy-MM-dd")
+    LocalDateTime updateDate;
+
+}
