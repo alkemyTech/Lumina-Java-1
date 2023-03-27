a59f64ffff44b36636a18f83dc8c83190b3253cc
gonza rolo <gonzarolo22@gmail.com>
2023-03-27 13:48:24 -0300

    elimine espacio

--- src/main/java/com/alkemy/wallet/entity/Role.java
+++ src/main/java/com/alkemy/wallet/entity/Role.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
 @Getter
