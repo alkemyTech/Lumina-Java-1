f31706ac26a7592b70e42db808e6a5935d1d2996
gonza rolo <gonzarolo22@gmail.com>
2023-03-27 17:15:09 -0300

    agrege el repo de Fix

---
+++ src/main/java/com/alkemy/wallet/repository/FixedTermDepositsRepository.java
@@ -0,0 +1,6 @@
+package com.alkemy.wallet.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FixedTermDepositsRepository extends JpaRepository<FixedTermDepositsRepository,Long> {
+}
