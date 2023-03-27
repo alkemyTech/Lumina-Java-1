b08ae47beed9e0d72f6ad9bc20c9141b2edfb0b6
carina-jg <carinagroisman@gmail.com>
2023-03-27 16:15:15 -0300

    se agrega relacion con account

--- src/main/java/com/alkemy/wallet/entity/Transaction.java
+++ src/main/java/com/alkemy/wallet/entity/Transaction.java
@@ -2,14 +2,16 @@ package com.alkemy.wallet.entity;
 
 import com.alkemy.wallet.enums.TransactionTypeEnum;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString
 @Entity
 @Table(name = "transactions")
@@ -20,18 +22,19 @@ public class Transaction {
     private Long id;
     @Column(name = "AMOUNT", nullable = false)
     private Double amount;
+    @Enumerated(EnumType.STRING)
     @Column(name = "TYPE", nullable = false)
     private TransactionTypeEnum type;
     @Column(name = "DESCRIPTION")
     private String description;
 
-    /*@ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     @JoinColumn(name = "ACCOUNT_ID", nullable = false)
-    private Account account;*/
+    private Account account;
 
     @Column(name="TRANSACTION_DATE")
     @JsonFormat(pattern="yyyy-MM-dd")
     LocalDateTime transactionDate;
-
 
 }
