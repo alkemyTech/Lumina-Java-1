700d9a5f651416db556f0bae8a86f52bded70d3c
gonza rolo <gonzarolo22@gmail.com>
2023-03-27 12:20:30 -0300

    creacion de Roles

---
+++ src/main/java/com/alkemy/wallet/entity/Role.java
@@ -0,0 +1,36 @@
+package com.alkemy.wallet.entity;
+
+import com.alkemy.wallet.enums.RoleEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private RoleEnum name;
+    private String description;
+
+    @CreationTimestamp
+    @Column(name="CREATION_DATE")
+    @JsonFormat(pattern="yyyy-MM-dd")
+    LocalDateTime creationDate;
+
+    @UpdateTimestamp
+    @Column(name="UPDATE_DATE")
+    @JsonFormat(pattern="yyyy-MM-dd")
+    LocalDateTime updateDate;
+}
