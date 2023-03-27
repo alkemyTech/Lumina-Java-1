793c4949a29331e232d6c9d84d96a6465f765596
gonza rolo <gonzarolo22@gmail.com>
2023-03-27 14:52:04 -0300

    reunion en grupo y modificacion de entidad

--- src/main/java/com/alkemy/wallet/entity/Role.java
+++ src/main/java/com/alkemy/wallet/entity/Role.java
@@ -8,8 +8,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -20,8 +23,13 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "NAME",nullable = false)
     private RoleEnum name;
+    @Column(name = "DESCRIPTION", nullable = true)
     private String description;
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    private Set<User> users= new HashSet<>();
 
     @CreationTimestamp
     @Column(name="CREATION_DATE")
