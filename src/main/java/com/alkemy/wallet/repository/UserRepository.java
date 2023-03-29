7690ae35f6055cb1aeae55d8503532228c248cf1
gonza rolo <gonzarolo22@gmail.com>
2023-03-29 17:37:31 -0300

    prueba de listado de usuarios

--- src/main/java/com/alkemy/wallet/repository/UserRepository.java
+++ src/main/java/com/alkemy/wallet/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.alkemy.wallet.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findOneByEmail(String email);
 }
