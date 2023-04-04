9f9b883d2fde8602c5d5723cc53d53f60f4c1564
gonza rolo <gonzarolo22@gmail.com>
2023-04-04 14:47:39 -0300

    subi tarjeta LJT1-20 listar cuentas de usuarios

--- src/main/java/com/alkemy/wallet/service/AccountService.java
+++ src/main/java/com/alkemy/wallet/service/AccountService.java
@@ -1,8 +1,31 @@
 package com.alkemy.wallet.service;
 
+import com.alkemy.wallet.entity.Account;
+import com.alkemy.wallet.entity.User;
+import com.alkemy.wallet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class AccountService {
+        @Autowired
+        private UserRepository userRepository;
 
-}
+        public List<Account> accountList(Long id) {
+            Optional<User> user = userRepository.findById(id);
+            try {
+                return user.get().getAccounts();
+            } catch (NoSuchElementException e) {
+                e.printStackTrace();
+                return new ArrayList<>();
+            }
+        }
+    }
+
+
+
