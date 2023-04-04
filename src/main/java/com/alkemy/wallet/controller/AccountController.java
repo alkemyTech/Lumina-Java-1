fc74797186050e739dfadc9d35bdfcf043de8f4a
gonza rolo <gonzarolo22@gmail.com>
2023-04-04 14:27:18 -0300

    tarjeta n°20

---
+++ src/main/java/com/alkemy/wallet/controller/AccountController.java
@@ -0,0 +1,28 @@
+package com.alkemy.wallet.controller;
+
+import com.alkemy.wallet.entity.Account;
+import com.alkemy.wallet.service.AccountService;
+import com.alkemy.wallet.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @Autowired
+    AccountService userService;
+
+
+    @GetMapping("/{id}")
+    ResponseEntity<List<Account>> accountList(@PathVariable Long id){
+        List<Account> accounts =userService.accountList(id);
+        return ResponseEntity.ok(accounts);
+    }
+
+
+}
