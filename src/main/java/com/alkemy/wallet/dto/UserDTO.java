6c87ce2f14373c82f24e3170d9704b930aaae7aa
carina-jg <carinagroisman@gmail.com>
2023-04-04 22:53:17 -0300

    se agrega endpoint para editar una transaccion

--- src/main/java/com/alkemy/wallet/dto/UserDTO.java
+++ src/main/java/com/alkemy/wallet/dto/UserDTO.java
@@ -1,13 +1,10 @@
 package com.alkemy.wallet.dto;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import com.alkemy.wallet.entity.Role;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
